5cd4d6e61dba632cba27a509d5d09e9f28d8cb78
Mark Wilgelm <markujke@mail.ru>
2022-07-19 23:18:08 +0200

    commit

--- BruteForce/Sources/Scenes/Controller/ViewController.swift
+++ BruteForce/Sources/Scenes/Controller/ViewController.swift
@@ -112,7 +112,7 @@ class ViewController: UIViewController, UpdateInterface {
     func updateInterface() {
         DispatchQueue.main.async {
             self.passwordTextField.isSecureTextEntry = false
-            self.labelText.text = self.passwordTextField.text
+            self.labelText.text = self.bruteForce.password
             self.activityIndicator.stopAnimating()
             self.generatePasswordButton.isEnabled = true
             
